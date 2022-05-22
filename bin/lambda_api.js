1c2d5289140a44bbebd6e652063d736a7f674726
azumbro <azumbro@users.noreply.github.com>
2022-05-22 15:40:23 -0700

    Add account settings to code

--- bin/lambda_api.js
+++ bin/lambda_api.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const cdk = require("aws-cdk-lib");
-const { LambdaAPIStack } = require("../lib/lambda_api-stack");
+const { LambdaAPIStack, ACCOUNT_SETTINGS } = require("../lib/lambda_api-stack");
 
 const app = new cdk.App();
-new LambdaAPIStack(app, "LambdaAPIStack", {})
+new LambdaAPIStack(app, "LambdaAPIStack", { env: ACCOUNT_SETTINGS })
